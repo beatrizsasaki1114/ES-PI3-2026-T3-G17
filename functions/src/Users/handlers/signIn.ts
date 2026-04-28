f0fd5e938cae75ed31ea39e624dfd0f45c5e98ad
Heloisa Lacerda <125582181+loisaaz@users.noreply.github.com>
2026-04-28 16:40:19 -0300

    Criado rota de login
    alterado os diretórios signIn e userRepository

--- functions/src/Users/handlers/signIn.ts
+++ functions/src/Users/handlers/signIn.ts
@@ -1,13 +1,48 @@
-import {setUser} from "../repositories/userRepository";
+/**
 import {CallableRequest, onCall} from "firebase-functions/v2/https";
 import type {Users} from "../types/index";
+ */
 
-export const signInUser = onCall(
-  {region: "southamerica-east1"},
-  async (request: CallableRequest<Users>)=> {
-    const {uid}= request.data;
-    return getUser({
-      uid: uid,
-    });
+import {getUser} from "../repositories/userRepository"; 
+import {onCall, HttpsError} from "firebase-functions/v2/https";
+
+export const signInUser = onCall({ region: "southamerica-east1" }, async (request) => {
+  const { email, password } = request.data; 
+
+  try {
+    // validar se os campos estão preenchidos
+    if (!email || !password) {
+      throw new HttpsError("invalid-argument", "E-mail e senha são obrigatórios.");
+    }
+
+    // busca no db o usuário pelo email 
+    const user = await getUser(email);
+
+    if (!user) {
+      // email não encontrado
+      throw new HttpsError("not-found", "Usuário não encontrado.");
+    }
+
+    const Password = password === user.senha; 
+
+    if (!Password) {
+      // senha estiver errada
+      throw new HttpsError("unauthenticated", "Senha incorreta.");
+    }
+
+    // retorno sucess
+    return {
+      uid: user.id,
+      nome: user.nome,
+      //token: "0"  
+    };
+
+  } catch (error) {
+    if (error instanceof HttpsError) {
+      throw error;
+    }
+    // erros genéricos
+      console.error("Erro no login:", error);
+      throw new HttpsError("internal", "Erro interno ao processar o login.");
   }
-);
+});
--- functions/src/Users/repositories/userRepository.ts
+++ functions/src/Users/repositories/userRepository.ts
@@ -1,6 +1,8 @@
 import {Users} from "../types";
 import {db} from "../../shared/firebase";
-
+//import { getDataConnect } from "firebase-admin/data-connect";
+//import { CollectionGroup } from "firebase-admin/firestore";
+//import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const usuariosCollection = db.collection("Usuários");
 /**
@@ -19,4 +21,19 @@ export async function setUser(data: Users) {
   });
 }
 
-export async function getUser
+export async function getUser(email: string) {
+  const querySnapshot = await usuariosCollection.where('email', '==', email).get();
+  if (querySnapshot.empty){
+    return null;
+  } 
+  const doc = querySnapshot.docs[0]; // o documento do usuario
+  const userData = doc.data(); // informações do doc
+
+  return {
+    // id do Firestore
+    id: doc.id,          
+    nome: userData.nome, 
+    email: userData.email,
+    senha: userData.senha
+  };
+}
